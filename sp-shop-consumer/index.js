f22ade7e217ae0a8028142d8efbf68c58a42c3b6
AbdelrahmanHany22 <boudyhany22@gmail.com>
2023-01-06 16:43:16 +0200

    idek test?

--- sp-shop-consumer/index.js
+++ sp-shop-consumer/index.js
@@ -1,3 +1,25 @@
 const { startKafkaConsumer } = require('./connectors/kafka');
+const express = require('express');
 
-startKafkaConsumer()
+const app = express();
+const cors = require('cors');
+//security dependencies
+const morgan = require('morgan');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(xss());
+// If request doesn't match any of the above routes then return 404
+app.use((req, res, next) => {
+    return res.status(404).send();
+  });
+  
+  // Create HTTP Server and Listen for Requests
+  app.listen(5010, async (req, res) => {
+    // Start Kafka consumer
+    await startKafkaConsumer();
+  });
+  
